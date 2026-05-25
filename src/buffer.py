8e75a5337f70daa5dec621b55705ce5134a33723
Mostafa Mehdizadeh <farzad.mehdizadeh.kh@gmail.com>
2026-05-25 17:46:06 +0330

    Update buffer.py

--- src/buffer.py
+++ src/buffer.py
@@ -1,1 +1,15 @@
+from collections import defaultdict, deque
 
+class FrameBuffer:
+
+    def __init__(self, max_len=16):
+        self.buffers = defaultdict(lambda: deque(maxlen=max_len))
+
+    def add(self, track_id, frame):
+        self.buffers[track_id].append(frame)
+
+    def get(self, track_id):
+        return self.buffers[track_id]
+
+    def ready(self, track_id, n=16):
+        return len(self.buffers[track_id]) == n
