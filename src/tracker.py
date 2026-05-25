073c8d84ae68f123347924dab89083f37c7b880a
Mostafa Mehdizadeh <farzad.mehdizadeh.kh@gmail.com>
2026-05-25 17:48:07 +0330

    Update tracker.py

--- src/tracker.py
+++ src/tracker.py
@@ -1,1 +1,15 @@
+class SimpleTracker:
 
+    def __init__(self):
+        self.next_id = 0
+
+    def update(self, detections):
+
+        tracked = []
+
+        for det in detections:
+
+            tracked.append((self.next_id, det))
+            self.next_id += 1
+
+        return tracked
