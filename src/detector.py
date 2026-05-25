394cdd7c5207ea3aa005e9aee60b3a8f6500f510
Mostafa Mehdizadeh <farzad.mehdizadeh.kh@gmail.com>
2026-05-25 17:46:36 +0330

    Update detector.py

--- src/detector.py
+++ src/detector.py
@@ -1,1 +1,25 @@
+from ultralytics import YOLO
 
+class PersonDetector:
+
+    def __init__(self):
+        self.model = YOLO("yolo11n.pt")
+
+    def detect(self, frame):
+
+        results = self.model(frame)[0]
+
+        persons = []
+
+        for box in results.boxes:
+
+            cls = int(box.cls[0])
+
+            if cls != 0:
+                continue
+
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+            persons.append((x1,y1,x2,y2))
+
+        return persons
