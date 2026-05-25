32ce7d489a45107d442f22bcb7c5fda74575c7cd
Mostafa Mehdizadeh <farzad.mehdizadeh.kh@gmail.com>
2026-05-25 17:46:59 +0330

    Update event_memory.py

--- src/event_memory.py
+++ src/event_memory.py
@@ -1,1 +1,16 @@
+from collections import defaultdict
 
+class EventMemory:
+
+    def __init__(self):
+        self.memory = defaultdict(list)
+
+    def update(self, track_id, label):
+
+        self.memory[track_id].append(label)
+
+        if len(self.memory[track_id]) > 10:
+            self.memory[track_id].pop(0)
+
+        return max(set(self.memory[track_id]),
+                   key=self.memory[track_id].count)
