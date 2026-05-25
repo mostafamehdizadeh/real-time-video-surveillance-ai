67655537c2a49a2c13a3e8439cf4eb7bb099a2fd
Mostafa Mehdizadeh <farzad.mehdizadeh.kh@gmail.com>
2026-05-25 17:47:22 +0330

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,1 +1,13 @@
+import json
+from datetime import datetime
 
+def log_event(track_id, label):
+
+    data = {
+        "time": str(datetime.now()),
+        "track_id": track_id,
+        "event": label
+    }
+
+    with open("events.json", "a") as f:
+        f.write(json.dumps(data) + "\n")
