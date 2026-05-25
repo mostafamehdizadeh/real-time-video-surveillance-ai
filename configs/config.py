85e81915fb20534a7d066e90828d87f3b34b9830
Mostafa Mehdizadeh <farzad.mehdizadeh.kh@gmail.com>
2026-05-25 17:45:00 +0330

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,1 +1,20 @@
+VIDEO_PATH = "0_te26.mp4"
+MODEL_PATH = "best_model.pth"
 
+IMG_SIZE = 224
+NUM_FRAMES = 16
+
+CLASS_NAMES = [
+    "safe_walkway_violation",
+    "unauthorized_intervention",
+    "opened_panel_cover",
+    "carrying_overload_with_forklift",
+    "safe_walkway",
+    "authorized_intervention",
+    "closed_panel_cover",
+    "safe_carrying"
+]
+
+UNSAFE_CLASSES = {
+    0,1,2,3
+}
